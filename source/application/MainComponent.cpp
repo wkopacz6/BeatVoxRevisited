bb1db95b64dc33fc6b847bfa415eb9b480391123
wkopacz6 <43361034+wkopacz6@users.noreply.github.com>
2022-02-10 00:27:16 -0500

    Add MainComponent and continue setting up cmake

---
+++ source/application/MainComponent.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Walter Kopacz on 2/10/22.
+//
+
+#include "MainComponent.h"
---
+++ source/application/MainComponent.h
@@ -0,0 +1,11 @@
+//
+// Created by Walter Kopacz on 2/10/22.
+//
+#pragma once
+
+#include "juce_core/juce_core.h"
+
+class MainComponent : public  juce::Component
+{
+
+};
